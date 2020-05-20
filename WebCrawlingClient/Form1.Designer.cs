3c409ff5cb56283917dcee4de25abc3dcf4821c0
Kaleb Askren <kra3s@mtmail.mtsu.edu>
2020-05-20 02:12:23 -0500

    second cmmit

---
+++ WebCrawlingClient/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace WebCrawlingClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textToDisplay1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textToDisplay2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textToDisplay3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.ListBoxLabel = new System.Windows.Forms.Label();
+            this.ClearFileBtn = new System.Windows.Forms.Button();
+            this.URLListBox = new System.Windows.Forms.ListBox();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.ListRefreshBtn = new System.Windows.Forms.Button();
+            this.BeginCrawl = new System.Windows.Forms.Button();
+            this.URLBox = new System.Windows.Forms.TextBox();
+            this.ControlBoxLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Description1 = new System.Windows.Forms.Label();
+            this.AuthorLabel1 = new System.Windows.Forms.Label();
+            this.DateOfPublicationLabel1 = new System.Windows.Forms.Label();
+            this.URLLabel1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WebCrawling-002";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.URLLabel1);
+            this.panel1.Controls.Add(this.DateOfPublicationLabel1);
+            this.panel1.Controls.Add(this.AuthorLabel1);
+            this.panel1.Controls.Add(this.Description1);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textToDisplay1);
+            this.panel1.Location = new System.Drawing.Point(15, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 358);
+            this.panel1.TabIndex = 1;
+            // 
+            // textToDisplay1
+            // 
+            this.textToDisplay1.AutoSize = true;
+            this.textToDisplay1.Location = new System.Drawing.Point(3, 17);
+            this.textToDisplay1.MaximumSize = new System.Drawing.Size(234, 150);
+            this.textToDisplay1.Name = "textToDisplay1";
+            this.textToDisplay1.Size = new System.Drawing.Size(0, 13);
+            this.textToDisplay1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Controls.Add(this.textToDisplay2);
+            this.panel2.Location = new System.Drawing.Point(255, 186);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(234, 358);
+            this.panel2.TabIndex = 2;
+            // 
+            // textToDisplay2
+            // 
+            this.textToDisplay2.AutoSize = true;
+            this.textToDisplay2.Location = new System.Drawing.Point(3, 4);
+            this.textToDisplay2.Name = "textToDisplay2";
+            this.textToDisplay2.Size = new System.Drawing.Size(35, 13);
+            this.textToDisplay2.TabIndex = 1;
+            this.textToDisplay2.Text = "label6";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel3.Controls.Add(this.textToDisplay3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(495, 186);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(234, 358);
+            this.panel3.TabIndex = 2;
+            // 
+            // textToDisplay3
+            // 
+            this.textToDisplay3.AutoSize = true;
+            this.textToDisplay3.Location = new System.Drawing.Point(3, 4);
+            this.textToDisplay3.Name = "textToDisplay3";
+            this.textToDisplay3.Size = new System.Drawing.Size(35, 13);
+            this.textToDisplay3.TabIndex = 2;
+            this.textToDisplay3.Text = "label6";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-3, -16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "WebPage1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "WebPage2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(492, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "WebPage3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add WebPage To List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.Controls.Add(this.ListBoxLabel);
+            this.panel4.Controls.Add(this.ClearFileBtn);
+            this.panel4.Controls.Add(this.URLListBox);
+            this.panel4.Controls.Add(this.exportButton);
+            this.panel4.Controls.Add(this.ListRefreshBtn);
+            this.panel4.Controls.Add(this.BeginCrawl);
+            this.panel4.Controls.Add(this.URLBox);
+            this.panel4.Controls.Add(this.ControlBoxLabel);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Location = new System.Drawing.Point(15, 42);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(714, 125);
+            this.panel4.TabIndex = 6;
+            // 
+            // ListBoxLabel
+            // 
+            this.ListBoxLabel.AutoSize = true;
+            this.ListBoxLabel.Location = new System.Drawing.Point(129, 50);
+            this.ListBoxLabel.Name = "ListBoxLabel";
+            this.ListBoxLabel.Size = new System.Drawing.Size(75, 13);
+            this.ListBoxLabel.TabIndex = 13;
+            this.ListBoxLabel.Text = "URLs to Crawl";
+            // 
+            // ClearFileBtn
+            // 
+            this.ClearFileBtn.Location = new System.Drawing.Point(4, 84);
+            this.ClearFileBtn.Name = "ClearFileBtn";
+            this.ClearFileBtn.Size = new System.Drawing.Size(109, 23);
+            this.ClearFileBtn.TabIndex = 12;
+            this.ClearFileBtn.Text = "Clear URL List File";
+            this.ClearFileBtn.UseVisualStyleBackColor = true;
+            this.ClearFileBtn.Click += new System.EventHandler(this.ClearFileBtn_Click);
+            // 
+            // URLListBox
+            // 
+            this.URLListBox.FormattingEnabled = true;
+            this.URLListBox.Location = new System.Drawing.Point(129, 66);
+            this.URLListBox.Name = "URLListBox";
+            this.URLListBox.Size = new System.Drawing.Size(104, 56);
+            this.URLListBox.TabIndex = 11;
+            this.URLListBox.SelectedIndexChanged += new System.EventHandler(this.URLListBox_SelectedIndexChanged);
+            this.URLListBox.DoubleClick += new System.EventHandler(this.URLListBox_DoubleClick);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(592, 99);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(119, 23);
+            this.exportButton.TabIndex = 10;
+            this.exportButton.Text = "Export Articles To File";
+            this.exportButton.UseVisualStyleBackColor = true;
+            // 
+            // ListRefreshBtn
+            // 
+            this.ListRefreshBtn.Location = new System.Drawing.Point(132, 13);
+            this.ListRefreshBtn.Name = "ListRefreshBtn";
+            this.ListRefreshBtn.Size = new System.Drawing.Size(101, 23);
+            this.ListRefreshBtn.TabIndex = 9;
+            this.ListRefreshBtn.Text = "Refresh Site List";
+            this.ListRefreshBtn.UseVisualStyleBackColor = true;
+            this.ListRefreshBtn.Click += new System.EventHandler(this.ListRefreshBtn_Click);
+            // 
+            // BeginCrawl
+            // 
+            this.BeginCrawl.Location = new System.Drawing.Point(239, 99);
+            this.BeginCrawl.Name = "BeginCrawl";
+            this.BeginCrawl.Size = new System.Drawing.Size(75, 23);
+            this.BeginCrawl.TabIndex = 8;
+            this.BeginCrawl.Text = "Begin Crawl";
+            this.BeginCrawl.UseVisualStyleBackColor = true;
+            this.BeginCrawl.Click += new System.EventHandler(this.BeginCrawl_Click);
+            // 
+            // URLBox
+            // 
+            this.URLBox.Location = new System.Drawing.Point(3, 42);
+            this.URLBox.Name = "URLBox";
+            this.URLBox.Size = new System.Drawing.Size(120, 20);
+            this.URLBox.TabIndex = 7;
+            // 
+            // ControlBoxLabel
+            // 
+            this.ControlBoxLabel.AutoSize = true;
+            this.ControlBoxLabel.Location = new System.Drawing.Point(328, 4);
+            this.ControlBoxLabel.Name = "ControlBoxLabel";
+            this.ControlBoxLabel.Size = new System.Drawing.Size(45, 13);
+            this.ControlBoxLabel.TabIndex = 6;
+            this.ControlBoxLabel.Text = "Controls";
+            this.ControlBoxLabel.Click += new System.EventHandler(this.ControlBoxLabel_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Header";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Description";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Author";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1, 256);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Date Of Publication";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "URL";
+            // 
+            // Description1
+            // 
+            this.Description1.AutoSize = true;
+            this.Description1.Location = new System.Drawing.Point(3, 98);
+            this.Description1.MaximumSize = new System.Drawing.Size(234, 150);
+            this.Description1.Name = "Description1";
+            this.Description1.Size = new System.Drawing.Size(41, 13);
+            this.Description1.TabIndex = 6;
+            this.Description1.Text = "label11";
+            // 
+            // AuthorLabel1
+            // 
+            this.AuthorLabel1.AutoSize = true;
+            this.AuthorLabel1.Location = new System.Drawing.Point(3, 232);
+            this.AuthorLabel1.MaximumSize = new System.Drawing.Size(234, 150);
+            this.AuthorLabel1.Name = "AuthorLabel1";
+            this.AuthorLabel1.Size = new System.Drawing.Size(41, 13);
+            this.AuthorLabel1.TabIndex = 7;
+            this.AuthorLabel1.Text = "label11";
+            // 
+            // DateOfPublicationLabel1
+            // 
+            this.DateOfPublicationLabel1.AutoSize = true;
+            this.DateOfPublicationLabel1.Location = new System.Drawing.Point(0, 269);
+            this.DateOfPublicationLabel1.MaximumSize = new System.Drawing.Size(234, 150);
+            this.DateOfPublicationLabel1.Name = "DateOfPublicationLabel1";
+            this.DateOfPublicationLabel1.Size = new System.Drawing.Size(41, 13);
+            this.DateOfPublicationLabel1.TabIndex = 8;
+            this.DateOfPublicationLabel1.Text = "label11";
+            // 
+            // URLLabel1
+            // 
+            this.URLLabel1.AutoSize = true;
+            this.URLLabel1.Location = new System.Drawing.Point(3, 322);
+            this.URLLabel1.MaximumSize = new System.Drawing.Size(234, 150);
+            this.URLLabel1.Name = "URLLabel1";
+            this.URLLabel1.Size = new System.Drawing.Size(41, 13);
+            this.URLLabel1.TabIndex = 9;
+            this.URLLabel1.Text = "label11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 556);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kaleb Askren Web-Crawler Update-001";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label textToDisplay1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label textToDisplay2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label textToDisplay3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label ControlBoxLabel;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button ListRefreshBtn;
+        private System.Windows.Forms.Button BeginCrawl;
+        private System.Windows.Forms.TextBox URLBox;
+        private System.Windows.Forms.ListBox URLListBox;
+        private System.Windows.Forms.Button ClearFileBtn;
+        private System.Windows.Forms.Label ListBoxLabel;
+        private System.Windows.Forms.Label URLLabel1;
+        private System.Windows.Forms.Label DateOfPublicationLabel1;
+        private System.Windows.Forms.Label AuthorLabel1;
+        private System.Windows.Forms.Label Description1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WebCrawlingClient/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Runtime.InteropServices;
+
+using System.IO;
+
+namespace WebCrawlingClient
+{
+    public partial class Form1 : Form
+    {
+        struct article
+        {
+            string headline;
+            string description;
+            string author;
+            string dateOfPublication;
+            string url;
+        }
+        Queue<String> UrlList = new Queue<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ControlBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Take URL from URLBox and add it to URL list file
+            //potentially add URL checking
+            using (StreamWriter w = File.AppendText("UrlList.txt"))
+            {
+                w.WriteLine(URLBox.Text);
+                w.Close();
+            }
+            URLBox.Text = "";
+           
+        }
+
+        private void BeginCrawl_Click(object sender, EventArgs e)
+        {
+            URLLabel1.Text = UrlList.ElementAt(0);
+            //washingtonPost appears to start headlines with "headline":" each time
+            //begin crawl of first three items in the list
+            int headerCharLength = 12;
+            int descriptionCharLength = 15;
+            int authorCharLength = 10;
+            foreach(string urlItem in UrlList)
+            {
+                //Temporary Variables to hold all item information
+                string tempHeader = "", tempDescription = "", tempAuthor = "", tempPubDate = "";
+
+                var client = new WebClient();
+                var text = client.DownloadString(urlItem);
+                //FIND HEADER:
+
+                var indexOfHeaderTag = text.LastIndexOf(@"" + '\u0022' + "headline" + '\u0022' + ":" + '\u0022');
+                //this index ^ is last index of the beginning of the headline title we should search until we reach another " which is represented by
+                //unicode \u0022
+                //12 is the size of the headline label in source code
+                
+                int indexForHeaderGet = indexOfHeaderTag + headerCharLength;
+                char curChar = text[indexForHeaderGet];
+
+                while (curChar != '\u0022')
+                {
+                    tempHeader = tempHeader + curChar.ToString();
+                    indexForHeaderGet++;
+                    curChar = text[indexForHeaderGet];
+
+                }
+                Console.WriteLine(tempHeader);
+                textToDisplay1.Text = tempHeader;
+
+                //FIND DESCRIPTION
+                var indexOfDescriptionTag = text.IndexOf(@"" + '\u0022' + "description" + '\u0022' + ":" + '\u0022');
+                int indexForDescriptionGet = indexOfDescriptionTag + descriptionCharLength;
+                curChar = text[indexForDescriptionGet];
+
+                while(curChar != '\u0022')
+                {                    
+                    tempDescription = tempDescription + curChar.ToString();
+
+                    
+                    indexForDescriptionGet++;
+                    curChar = text[indexForDescriptionGet];
+                }
+                Console.WriteLine(tempDescription);
+                Description1.Text = tempDescription;
+
+                //Finding the Author
+                var indexofAuthorTag = text.IndexOf(@"" + '\u0022' + "author" + '\u0022' + ":" + '\u0022');
+                int indexForAuthorGet = indexofAuthorTag + authorCharLength;
+                curChar = text[indexForAuthorGet];
+                while(curChar != '\u0022')
+                {
+                    tempAuthor = tempAuthor + curChar.ToString();
+                    indexForAuthorGet++;
+                    curChar = text[indexForAuthorGet];
+                }
+                Console.WriteLine(tempAuthor);
+                AuthorLabel1.Text = tempAuthor;
+            }
+
+
+        }
+
+        private void ClearFileBtn_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText(@"UrlList.txt", string.Empty);
+            URLListBox.Items.Clear();
+        }
+
+        private void URLListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void URLListBox_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(URLListBox.GetItemText(URLListBox.SelectedItem));
+
+        }
+
+        private void ListRefreshBtn_Click(object sender, EventArgs e)
+        {
+            URLListBox.Items.Clear();
+            string line;
+            // Read the file and display it line by line.  
+            //first get all of the URLs from the URL List File
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"UrlList.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                UrlList.Enqueue(line);
+                URLListBox.Items.Add(line);
+            }
+            file.Close();
+        }
+    }
+}
